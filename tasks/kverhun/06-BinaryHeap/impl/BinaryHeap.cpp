af038068012a05bb633678bc67a3b21f210ea634
kverhun <k.verhun@gmail.com>
2016-10-23 18:47:21 +0300

    BinaryHeap - fix pop behavior

--- tasks/kverhun/06-BinaryHeap/impl/BinaryHeap.cpp
+++ tasks/kverhun/06-BinaryHeap/impl/BinaryHeap.cpp
@@ -19,6 +19,7 @@ size_t BinaryHeap::Pop()
     auto root_element = m_elements[1];
     m_elements[1] = m_elements.back();
     m_elements.erase(m_elements.end() - 1, m_elements.end());
+    _FixupOrderAsParent(1);
     return root_element;
 }
 
@@ -45,11 +46,14 @@ void BinaryHeap::_FixupOrderAsParent(size_t i_element_index)
     while (children_indexes.first < heap_size)
     {
         auto max_children_index = children_indexes.first;
-        if (children_indexes.second < heap_size)
-            max_children_index = std::max(m_elements[children_indexes.first], m_elements[children_indexes.second]);
+        if (children_indexes.second < heap_size && m_elements[children_indexes.first] < m_elements[children_indexes.second])
+            max_children_index = children_indexes.second;
 
         if (m_elements[i_element_index] < m_elements[max_children_index])
             std::swap(m_elements[i_element_index], m_elements[max_children_index]);
+
+        i_element_index = max_children_index;
+        children_indexes = _GetChildrenIndexes(i_element_index);
     }
 }
 
--- tasks/kverhun/06-BinaryHeap/unit_tests/binary_heap_tests.cpp
+++ tasks/kverhun/06-BinaryHeap/unit_tests/binary_heap_tests.cpp
@@ -3,7 +3,7 @@
 
 #include "../impl/BinaryHeap.h"
 
-TEST_CASE("SomeTest")
+TEST_CASE("BinaryHeapTest1")
 {
     BinaryHeap heap;
 
@@ -27,5 +27,33 @@ TEST_CASE("SomeTest")
     REQUIRE(heap_vec[2] == 6);
     REQUIRE(heap_vec[3] == 4);
     REQUIRE(heap_vec[4] == 5);
+}
+
+TEST_CASE("BinaryHeapTest2")
+{
+    BinaryHeap heap;
+
+    heap.Insert(4);
+    heap.Insert(5);
+    heap.Insert(6);
+
+    auto heap_vec = heap.GetHeap();
+    REQUIRE(heap_vec[1] == 6);
+    REQUIRE(heap_vec[2] == 4);
+    REQUIRE(heap_vec[3] == 5);
+    
+    heap.Insert(7);
+    heap_vec = heap.GetHeap();
+    REQUIRE(heap_vec[1] == 7);
+
+    auto root = heap.Pop();
+    REQUIRE(root == 7);
+
+    heap_vec = heap.GetHeap();
+    REQUIRE(heap_vec[1] == 6);
+    REQUIRE(heap_vec[2] == 4);
+    REQUIRE(heap_vec[3] == 5);
+
+
 
 }
