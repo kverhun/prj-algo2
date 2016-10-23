3dd37b33b5b32ba304ebc86501a40a1bea0abaff
kverhun <k.verhun@gmail.com>
2016-10-23 19:05:36 +0300

    Binary heap - expand unit test

--- tasks/kverhun/06-BinaryHeap/unit_tests/binary_heap_tests.cpp
+++ tasks/kverhun/06-BinaryHeap/unit_tests/binary_heap_tests.cpp
@@ -1,32 +1,40 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
+#include <limits.h>
+
 #include "../impl/BinaryHeap.h"
+
+static const size_t g_zero_element_heap = std::numeric_limits<size_t>::max();
+
+namespace
+{
+    void _AreEqual(const std::vector<size_t>& i_vector, const std::initializer_list<size_t>& i_list)
+    {
+        auto it_vec = i_vector.begin();
+        auto it_list = i_list.begin();
+        for (; it_vec < i_vector.end() && it_list < i_list.end(); ++it_vec, ++it_list)
+        {
+            REQUIRE(*it_vec == *it_list);
+        }
+    }
+}
 
 TEST_CASE("BinaryHeapTest1")
 {
     BinaryHeap heap;
 
     heap.Insert(5);
-    auto heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 5);
+    _AreEqual(heap.GetHeap(), {g_zero_element_heap, 5});
+    
     heap.Insert(6);
-    heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 6);
-    REQUIRE(heap_vec[2] == 5);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 6, 5 });
 
     heap.Insert(4);
-    heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 6);
-    REQUIRE(heap_vec[2] == 5);
-    REQUIRE(heap_vec[3] == 4);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 6,5,4 });
 
     heap.Insert(7);
-    heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 7);
-    REQUIRE(heap_vec[2] == 6);
-    REQUIRE(heap_vec[3] == 4);
-    REQUIRE(heap_vec[4] == 5);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 7,6,4,5 });
 }
 
 TEST_CASE("BinaryHeapTest2")
@@ -37,23 +45,19 @@ TEST_CASE("BinaryHeapTest2")
     heap.Insert(5);
     heap.Insert(6);
 
-    auto heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 6);
-    REQUIRE(heap_vec[2] == 4);
-    REQUIRE(heap_vec[3] == 5);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 6, 4, 5 });
     
     heap.Insert(7);
-    heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 7);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 7, 6, 5, 4 });
 
-    auto root = heap.Pop();
-    REQUIRE(root == 7);
+    REQUIRE(heap.Pop() == 7);
 
-    heap_vec = heap.GetHeap();
-    REQUIRE(heap_vec[1] == 6);
-    REQUIRE(heap_vec[2] == 4);
-    REQUIRE(heap_vec[3] == 5);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 6, 4 ,5 });
 
+    heap.Insert(7);
+    heap.Insert(8);
+    heap.Insert(9);
 
-
+    REQUIRE(heap.Pop() == 9);
+    _AreEqual(heap.GetHeap(), { g_zero_element_heap, 8, 7, 5, 4, 6 });
 }
