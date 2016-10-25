75dde6be36694b0d8810ddfb860ab2bd03ee5403
kverhun <k.verhun@gmail.com>
2016-10-25 23:51:33 +0300

    Dijkstra - add unit test

--- tasks/kverhun/07-Graphs/unit_tests/graphs_tests.cpp
+++ tasks/kverhun/07-Graphs/unit_tests/graphs_tests.cpp
@@ -50,3 +50,22 @@ TEST_CASE("ShortestPath_Dijkstra_Test3")
     auto sh_path_24 = GetShortestPathDijkstra(graph, 2, 4);
     REQUIRE(45. == sh_path_24.second);
 }
+
+TEST_CASE("ShortestPath_Dijkstra_Test4")
+{
+    Graph graph = Graph(
+        {1, 2, 3, 4, 5, 6},
+        { {1, 2, 5}, {1, 4, 15}, {1, 5, 60}, {2, 3, 5}, {3, 4, 10}, {5, 4, 5}, {4, 6, 100}, {5, 6, 20} }
+    );
+
+    auto sh_path_16 = GetShortestPathDijkstra(graph, 1, 6);
+    auto sh_path_14 = GetShortestPathDijkstra(graph, 1, 4);
+    auto sh_path_46 = GetShortestPathDijkstra(graph, 4, 6);
+    auto sh_path_56 = GetShortestPathDijkstra(graph, 5, 6);
+
+    REQUIRE(80. == sh_path_16.second);
+    REQUIRE(15. == sh_path_14.second);
+    REQUIRE(100. == sh_path_46.second);
+    REQUIRE(20. == sh_path_56.second);
+
+}
